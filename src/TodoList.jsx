6ea109ba41994f2314e34d55dd788147aaf109ba
JHeroGR <jheroytc@gmail.com>
2026-04-17 03:26:57 -0500

    finished basic todo list app

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(['feed the dog', 'feed the cat', 'feed the bat']);
     const [newTask, setNewTask] = useState('');
 
     function handleInputChange(event) {
@@ -24,19 +24,25 @@ function TodoList() {
     }
 
     function deleteTask(index) {
-       
-    }
-
-    function updateTask(index, newTask) {
-
+       const updatedTodos = todos.filter((_, i) => i !== index);
+       setTodos(updatedTodos);
     }
 
     function moveTaskUp(index) {
-    
+        if (index > 0) {
+            const updatedTodos = [...todos];
+            [updatedTodos[index], updatedTodos[index - 1]] = [updatedTodos[index - 1], updatedTodos[index]];
+            setTodos(updatedTodos);
+        }
     }
 
     function moveTaskDown(index) {
-    
+        if (index < todos.length - 1) {
+            const updatedTodos = [...todos];
+            [updatedTodos[index], updatedTodos[index + 1]] = [updatedTodos[index + 1], updatedTodos[index]];
+            setTodos(updatedTodos);
+
+        }
     }
 
     return(
@@ -46,21 +52,26 @@ function TodoList() {
             <h3 className="alert-text">Please enter a task.</h3>
 
             <h3>Enter New Task</h3>
-            <input type="text" placeholder='Enter a new task...' value={newTask} onChange={handleInputChange} />
-            <input type="button" value="Add Task" onClick={addTask} />
+            <input type="text" 
+                   placeholder='Enter a new task...' 
+                   value={newTask} 
+                   onChange={handleInputChange} />
 
+            <button className="add-button"
+                    onClick={addTask}>Add</button>
             <h3>Tasks</h3>
             
-            <ul>
-                {todos.map((item, todos) => (
+            <ol>
+                {todos.map((todo, index) =>
                     <li key={index}>
-                        <span className='text'>{item}</span>
-                        <button className='delete-button' onClick={deleteTask(index)}>Delete</button>
-                        <button className='move-up-button' onClick={moveTaskUp(index)}>Move up</button>
-                        <button className='move-down-button' onClick={moveTaskDown(index)}>Move down</button>
+                        <span className='text'>{todo}</span>
+                        <button className='delete-button' onClick={() => deleteTask(index)}>Delete</button>
+                        <button className='move-button' onClick={() =>moveTaskUp(index)}>👆</button>
+                        <button className='move-button' onClick={() =>moveTaskDown(index)}>👇</button>
                     </li>
-                ))}
-            </ul>
+                )}
+            </ol>
+           
         </div>
     );
 }
